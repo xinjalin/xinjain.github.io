3745759017d72e1a407533625b3dc34a6c7e9170
xinjalin <xinjalin@gmail.com>
2024-12-04 19:50:03 +1030

    typed-text effect on summary text

--- js/script.js
+++ js/script.js
@@ -26,6 +26,44 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
+// typed-text
+// set up text to print, each item in array is new line
+const aText = new Array(
+    "Hi, I'm Christopher, a Software Developer and Tableau Data Specialist with 2 years experience in (web dev, data analysis / visualization, devops, AWS, documentation, etc). Free software enthusiast, and avid video game player."
+);
+const iSpeed = 30; // time delay of print out
+const iIndex = 0; // start printing array at this position
+const iArrLength = aText[0].length; // the length of the text array
+const iScrollAt = 20; // start scrolling up at this many lines
+
+let iTextPos = 0; // initialize text position
+let sContents = ""; // initialize contents variable
+let iRow; // initialize current row
+
+function typewriter() {
+    sContents = " ";
+    iRow = Math.max(0, iIndex - iScrollAt);
+    const destination = document.getElementById("typed-text");
+
+    while (iRow < iIndex) {
+        sContents += aText[iRow++] + "<br />";
+    }
+    destination.innerHTML =
+        sContents + aText[iIndex].substring(0, iTextPos) + "_";
+    if (iTextPos++ == iArrLength) {
+        iTextPos = 0;
+        iIndex++;
+        if (iIndex != aText.length) {
+            iArrLength = aText[iIndex].length;
+            setTimeout("typewriter()", 500);
+        }
+    } else {
+        setTimeout("typewriter()", iSpeed);
+    }
+}
+
+typewriter();
+
 // search posts
 const searchInput = document.getElementById("search-input");
 const postList = document.getElementById("post-list");
